d3c2db17abbd5b7074647ae37d49250fee2c4df3
yousef <yousefshwaly@gmail.com>
2025-11-05 16:57:46 +0200

    Create Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AppLayout from "./myComponents/AppLayout";
+import Features from "./pages/Features";
+import Reviews from "./pages/Reviews";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div>
-      <AppLayout />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="home" element={<Home />} />
+          <Route path="features" element={<Features />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
